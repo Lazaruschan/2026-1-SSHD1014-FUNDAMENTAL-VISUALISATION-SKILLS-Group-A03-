c1bad7a0a9f82c65bd4b902bfd06c5c40728b5b9
lazarus <lazarus@lazaruss-MacBook-Air.local>
2026-09-12 15:36:41 +0800

    Publish SSHD1014 Fundamental Visualisation Skills course website.

--- auth.js
+++ auth.js
@@ -10,6 +10,7 @@
   const PASSWORD_HASH =
     "42ae5e4320ec6866cfdc6fd5fccb3d50354032aa0730bbb49a8487cc6fb8ba88";
   const STORAGE_KEY = "sshd1014_site_unlocked";
+  // Course code must be SSHD1014 (not SHDH1014)
   const TITLE = "SSHD1014 Fundamental Visualisation Skills";
 
   function inSlidesFolder() {
